71dc3766cde27b93f2ee75bd6993cbb4eea00e55
Kristina Fischer <kristinafischer2345@gmail.com>
2025-10-22 14:11:15 -0700

    Adds chain configs for synd mainnets Stadium, CMMT, and Clankermon

---
+++ constants/additionalChainRegistry/chainid-574014.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "Stadium",
+  "chain": "Stadium",
+  "shortName": "stadium",
+  "infoURL": "https://playstadium.gg",
+  "icon": "stadium",
+  "chainId": 574014,
+  "networkId": 574014,
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "nativeCurrency": {
+    "name": "USDC",
+    "symbol": "USDC",
+    "decimals": 18
+  },
+  "rpc": ["https://rpc.playstadium.gg"],
+  "faucets": [],
+  "explorers": [
+    {
+      "name": "Stadium Explorer",
+      "url": "https://explorer.playstadium.gg",
+      'logo': 'blockscout',
+      "standard": "EIP3091"
+    }
+  ]
+}
---
+++ constants/additionalChainRegistry/chainid-63829.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "CMMT",
+  "chain": "CMMT",
+  "shortName": "cmmt",
+  "infoURL": "https://cmmt.ai",
+  "icon": "cmmt",
+  "chainId": 63829,
+  "networkId": 63829,
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "nativeCurrency": {
+    "name": "Syndicate",
+    "symbol": "SYND",
+    "decimals": 18
+  },
+  "rpc": ["https://cmmt.rpc.syndicate.io"],
+  "faucets": [],
+  "explorers": [
+    {
+      "name": "CMMT Explorer",
+      "url": "https://cmmt.explorer.syndicate.io",
+      'logo': 'blockscout',
+      "standard": "EIP3091"
+    }
+  ]
+}
